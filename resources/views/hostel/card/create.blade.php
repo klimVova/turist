907acd47fe410d124149d33029a9b4efa4f2c55e
klimvov <klimashevskijvova@gmail.com>
2023-03-02 15:36:23 +0300

    update catalog card

--- resources/views/hostel/card/create.blade.php
+++ resources/views/hostel/card/create.blade.php
@@ -163,7 +163,7 @@
                                 </select>
                             </div>
                             <div class="form-group w-50">
-                                <label>Тэги</label>
+                                <label>Услуги в номере</label>
                                 <select class="select2" name="hostel_cardTag_ids[]" multiple="multiple"
                                         data-placeholder="Выберите тэги" style="width: 100%;">
                                     @foreach($hostelCardTags as $hostelCardTag)
