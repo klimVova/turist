b96581732a0d8d32b69b159ceba5a8c68352eeb8
klimvova <klimashevskijvova@gmail.com>
2023-07-05 16:36:45 +0300

    up menu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,6 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Почта') }}</label>
 
@@ -65,6 +64,34 @@
                             </div>
                         </div>
                     </form>
+                    <ul class=" navbar-nav col-md-8 offset-md-4">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="mt-3 btn btn-primary" href="{{ route('register') }}">{{ __('Зарегистрироваться') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
             </div>
         </div>
