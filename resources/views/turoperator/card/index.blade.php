ad62e078edf8edc15fa53aec6a1213bdf25d41e3
klimvova <klimashevskijvova@gmail.com>
2023-08-07 13:57:42 +0300

    instr

--- resources/views/turoperator/card/index.blade.php
+++ resources/views/turoperator/card/index.blade.php
@@ -6,7 +6,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Карточка отеля</h1>
+                        <h1 class="m-0">Карточка туристического агентства</h1>
                     </div><!-- /.col -->
 
                 </div><!-- /.row -->
