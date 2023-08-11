745a936c1f18d6f8da51a70fbb8f8caa4c7a351b
klimvova <klimashevskijvova@gmail.com>
2023-08-11 16:24:40 +0300

    conS

--- resources/js/views/cafeCard.vue
+++ resources/js/views/cafeCard.vue
@@ -18,7 +18,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="card-main-info col col-12 col-md-9">
-                                <div class="card-item-img x">
+                                <div class="card-item-img ">
                                     <img :src="card.image_url">
                                 </div>
                                 <div class="card-item-descr">
