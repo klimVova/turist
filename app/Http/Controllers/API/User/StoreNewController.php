46916d48aa0b929aabcb46bf96a7f6e06ea31552
klimvova <klimashevskijvova@gmail.com>
2023-09-20 20:13:28 +0300

    consult

--- app/Http/Controllers/API/User/StoreNewController.php
+++ app/Http/Controllers/API/User/StoreNewController.php
@@ -21,17 +21,8 @@ class StoreNewController extends Controller
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'email_verified_at' => $data['email_verified_at'],
             'password' => Hash::make(  $data['password'] ),
-            'city_id' => $data['city_id'],
-            'republic_id' =>$data['republic_id'],
-            'district_id' => $data['district_id'],
-            'role' => $data['role'],
-            'age' => $data['age'],
-            'phone' => $data['phone'],
-            'gender' => $data['gender'],
-            'user_city' => $data['user_city'],
-            'user_district' => $data['user_district'],
+
         ]);
 
         return new UserResource($user);
