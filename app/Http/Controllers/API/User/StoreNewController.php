fbb721814a3d798c4c05cae6471aaabd74d0437e
klimvova <klimashevskijvova@gmail.com>
2023-09-20 20:22:06 +0300

    consult

--- app/Http/Controllers/API/User/StoreNewController.php
+++ app/Http/Controllers/API/User/StoreNewController.php
@@ -4,14 +4,9 @@ namespace App\Http\Controllers\API\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Medical\CommentStoreRequest;
-use App\Http\Resources\PreOrder\PreOrderResource;
 use App\Http\Resources\User\UserResource;
-use App\Models\CommentMed;
-use App\Models\MedicalCard;
-use App\Models\preOrder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use function response;
 
 class StoreNewController extends Controller
 {
@@ -22,6 +17,15 @@ class StoreNewController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make(  $data['password'] ),
+            'city_id' => NUll,
+            'republic_id' => NUll,
+            'district_id' => NUll,
+            'role' => 1,
+            'age' => NUll,
+            'phone' => NUll,
+            'gender' => NUll,
+            'user_city' => NUll,
+            'user_district' => NUll,
 
         ]);
 
