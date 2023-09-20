50219943ad26ed54a041ec4f7a95f67011d28749
klimvova <klimashevskijvova@gmail.com>
2023-09-20 21:18:17 +0300

    consult

--- app/Http/Controllers/API/User/StoreNewController.php
+++ app/Http/Controllers/API/User/StoreNewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Medical\CommentStoreRequest;
+use App\Http\Requests\API\User\NewUserRequest;
 use App\Http\Resources\PreOrder\PreOrderResource;
 use App\Http\Resources\User\UserResource;
 use App\Models\CommentMed;
@@ -15,23 +16,14 @@ use function response;
 
 class StoreNewController extends Controller
 {
-    public function __invoke(CommentStoreRequest $request)
+    public function __invoke(NewUserRequest $request)
     {
         $data = $request->validated();
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'email_verified_at' => $data['email_verified_at'],
             'password' => Hash::make(  $data['password'] ),
-            'city_id' => $data['city_id'],
-            'republic_id' =>$data['republic_id'],
-            'district_id' => $data['district_id'],
-            'role' => $data['role'],
-            'age' => $data['age'],
-            'phone' => $data['phone'],
-            'gender' => $data['gender'],
-            'user_city' => $data['user_city'],
-            'user_district' => $data['user_district'],
+
         ]);
 
         return new UserResource($user);
