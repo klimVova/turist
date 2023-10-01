75551453ca730834c51d025162b653b7352a6356
klimvova <klimashevskijvova@gmail.com>
2023-10-01 17:55:19 +0300

    mob up

--- app/Http/Controllers/API/Payment/StoreController.php
+++ app/Http/Controllers/API/Payment/StoreController.php
@@ -26,7 +26,7 @@ class StoreController extends Controller
 
 
 
-        $totals = DB::table('pre_orders')->where('user_id', '=', $user['id'])->pluck('total_price');
+        $totals = DB::table('pre_orders')->where('user_id', '=', $id)->pluck('total_price');
 
         return $totals;
 
