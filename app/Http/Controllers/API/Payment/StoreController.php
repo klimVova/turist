b0e4ec075587932558f312d952408425aaf8286c
klimvova <klimashevskijvova@gmail.com>
2023-10-01 16:52:35 +0300

    mob up

--- app/Http/Controllers/API/Payment/StoreController.php
+++ app/Http/Controllers/API/Payment/StoreController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Payment;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
-use PhpParser\Error;
 use Voronkovich\SberbankAcquiring\Client;
 use Voronkovich\SberbankAcquiring\Currency;
 use Voronkovich\SberbankAcquiring\HttpClient\HttpClientInterface;
@@ -15,19 +14,15 @@ class StoreController extends Controller
 {
     public function __invoke($id)
     {
-        try{
-            $client = new Client([
-                'userName' => 't7717762693-api',
-                'password' => 't7717762693',
-                'apiUri' => Client::API_URI_TEST,
-                'currency' => Currency::RUB,
-                'httpMethod' => HttpClientInterface::METHOD_GET,
-            ]);
-            return $client;
-        }
-        catch(error){
-            return 'lalala';
-        }
+        $client = new Client([
+            'userName' => 't7717762693-api',
+            'password' => 't7717762693',
+            'apiUri' => Client::API_URI_TEST,
+            'currency' => Currency::RUB,
+            'httpMethod' => HttpClientInterface::METHOD_GET,
+        ]);
+
+        return response()->json($client);
 //        $user = DB::table('users')->where('id', '=', $id)->get();
 //        $totals = DB::table('pre_orders')->where('user_id', '=', $user['id'])->pluck('total_price');
 //        foreach (array($totals) as $total)
@@ -88,6 +83,6 @@ class StoreController extends Controller
 //
 //        $response = $client->registerOrder($payment->id, $amount, $returnUrl, $data);
 
-
+        return $response;
     }
 }
