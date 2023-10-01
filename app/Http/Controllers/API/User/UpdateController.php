b90954f4801a2a62ab111e629ba09a6af92f798a
klimvova <klimashevskijvova@gmail.com>
2023-10-01 15:37:38 +0300

    mob up

--- app/Http/Controllers/API/User/UpdateController.php
+++ app/Http/Controllers/API/User/UpdateController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\User\UpdateRequest;
 use App\Http\Requests\API\User\UpUserRequest;
-use App\Http\Resources\User\UserResource;
 use App\Models\User;
 
 class UpdateController extends Controller
