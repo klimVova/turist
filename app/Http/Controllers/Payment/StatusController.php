92781f81db9c450ed07f7ae9ad23687c9545762e
klimvova <klimashevskijvova@gmail.com>
2023-10-04 19:18:22 +0300

    mob up

--- app/Http/Controllers/Payment/StatusController.php
+++ app/Http/Controllers/Payment/StatusController.php
@@ -16,7 +16,7 @@ class StatusController extends Controller
 
         $data = $request->all();
 
-        Log::info(json_encode($data));
+     //   Log::info(json_encode($data));
 
         $orderId = $data['orderNumber'];
         $status = $data['status'];
