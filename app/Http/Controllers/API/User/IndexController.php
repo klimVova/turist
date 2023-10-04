457cfed239dac8e34587b0102c7b018d5009e184
klimvova <klimashevskijvova@gmail.com>
2023-10-04 15:16:17 +0300

    mob up

--- app/Http/Controllers/API/User/IndexController.php
+++ app/Http/Controllers/API/User/IndexController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\API\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\API\User\IndexRequest;
-use App\Http\Resources\Spa\SpaItemResource;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
