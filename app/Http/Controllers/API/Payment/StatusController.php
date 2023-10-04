6cef7f793c4665dbfb7dc56b000d8ea102c4b757
klimvova <klimashevskijvova@gmail.com>
2023-10-04 16:48:23 +0300

    mob up

--- app/Http/Controllers/API/Payment/StatusController.php
+++ app/Http/Controllers/API/Payment/StatusController.php
@@ -9,23 +9,22 @@ use Illuminate\Http\Request;
 
 class StatusController extends Controller
 {
-    public function __invoke( Request $request , User $id)
+    public function __invoke( Request $request)
     {
-        return 'zxczxczxc';
-//        $data = $request->all();
-//
-//        $orderId = $data['orderNumber'];
-//        $status = $data['status'];
-//
-//        if ($status == 1) {
-//            $payment = Payment::where('id', $orderId)->first();
-//            $promo = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 8);
-//            if($payment){
-//                $payment->status = 1;
-//                $payment->promocode = $promo;
-//                $payment->save();
-//            }
-//        }
-//        return response()->json([]);
+        $data = $request->all();
+
+        $orderId = $data['orderNumber'];
+        $status = $data['status'];
+
+        if ($status == 1) {
+            $payment = Payment::where('id', $orderId)->first();
+            $promo = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 8);
+            if($payment){
+                $payment->status = 1;
+                $payment->promocode = $promo;
+                $payment->save();
+            }
+        }
+        return response()->json([]);
     }
 }
