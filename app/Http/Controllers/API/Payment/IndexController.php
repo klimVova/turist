29320275b48ec0273c671e82e1a55f9172f9d48a
klimvova <klimashevskijvova@gmail.com>
2023-10-06 00:00:50 +0300

    mob up

--- app/Http/Controllers/API/Payment/IndexController.php
+++ app/Http/Controllers/API/Payment/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\Payment;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Payment\IndexResource;
 use App\Models\Payment;
+use Illuminate\Support\Facades\DB;
 
 
 class IndexController extends Controller
@@ -12,9 +13,8 @@ class IndexController extends Controller
     public function __invoke()
     {
 
-
-        $payment= Payment::all();
-
+        $payment= DB::table('payments')->get();
         return  IndexResource::collection($payment);
     }
 }
+
