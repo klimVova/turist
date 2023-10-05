bb4a0a958fd6e5847a84acc90b9a73be525640f8
klimvova <klimashevskijvova@gmail.com>
2023-10-05 23:26:28 +0300

    mob up

--- app/Http/Controllers/API/Payment/StatusController.php
+++ app/Http/Controllers/API/Payment/StatusController.php
@@ -12,13 +12,12 @@ class StatusController extends Controller
     public function __invoke($id)
     {
         $payment = Payment::where('user_id',  $id)->orderBy('id', 'desc')->first();
-
         $promo = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 8);
-
         if($payment){
             $payment->status = 1;
             $payment->promocode = $promo;
             $payment->save();
         }
+        return response()->json([]);
     }
 }
