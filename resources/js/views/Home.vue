574f9fd5dac762081bd766e408438073518dce7d
klimVova <87908301+klimVova@users.noreply.github.com>
2023-12-04 18:06:58 +0300

    up home

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -31,6 +31,83 @@
             <p><b>ТурМедДрайвер - это здоровый и комфортный мир!</b></p>
         </div>
     </div>
+    <div class="container-fluid mp-services-blocks">
+        <div class="row">
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'med'}">
+                    <div class="section-item">
+                        <img src="assets/img/med-main.png">
+                        <p>Медицинские центры</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    active-class="active"
+                    :to="{name:'spa'}">
+                    <div class="section-item">
+                        <img src="assets/img/spa-main.png">
+                        <p>SPA/Бьюти</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'cafe'}">
+                    <div class="section-item">
+                        <img src="assets/img/cafe-main.png">
+                        <p>Рестораны/Кафе</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'hotel'}">
+                    <div class="section-item">
+                        <img src="assets/img/hotel-main.png">
+                        <p>Отели</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'tur'}">
+                    <div class="section-item">
+                        <img src="assets/img/tour-main.png">
+                        <p>Туроператоры</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'sanatorium'}">
+                    <div class="section-item">
+                        <img src="assets/img/san-main.png">
+                        <p>Санатории</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'sport'}">
+                    <div class="section-item">
+                        <img src="assets/img/sport-main.png">
+                        <p>Спорт</p>
+                    </div>
+                </router-link>
+            </div>
+            <div class="col col-12 col-md-3">
+                <router-link
+                    :to="{name:'constaling'}">
+                    <div class="section-item">
+                        <img src="assets/img/cons-main.png">
+                        <p>Консалтинг</p>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </div>
     <blog></blog>
 </template>
 
