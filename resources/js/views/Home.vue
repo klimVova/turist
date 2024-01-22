7c5b56c2409e24636f68db48033b55d7945779b9
klimVova <87908301+klimVova@users.noreply.github.com>
2024-01-22 15:40:39 +0300

    main page

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,5 +1,80 @@
 <template>
     <div class="container-fluid slider-block">
+        <div class="row main-cat-blocks">
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			<router-link
+                    :to="{name:'med'}">
+                    <div class="section-item">
+                        <img src="assets/img/med-main.png">
+                        <p>Медицинские центры</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			 <router-link
+                    active-class="active"
+                    :to="{name:'spa'}">
+                    <div class="section-item">
+                        <img src="assets/img/spa-main.png">
+                        <p>SPA/Бьюти</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			 <router-link
+                    :to="{name:'hotel'}">
+                    <div class="section-item">
+                        <img src="assets/img/hotel-main.png">
+                        <p>Отели</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			 <router-link
+                    :to="{name:'sport'}">
+                    <div class="section-item">
+                        <img src="assets/img/sport-main.png">
+                        <p>Спорт</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			 <router-link
+                    :to="{name:'tur'}">
+                    <div class="section-item">
+                        <img src="assets/img/tour-main.png">
+                        <p>Туроператоры</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			 <router-link
+                    :to="{name:'sanatorium'}">
+                    <div class="section-item">
+                        <img src="assets/img/san-main.png">
+                        <p>Санатории</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			<router-link
+                    :to="{name:'cafe'}">
+                    <div class="section-item">
+                        <img src="assets/img/cafe-main.png">
+                        <p>Рестораны/Кафе</p>
+                    </div>
+                </router-link>
+		</a></div>
+		<div class=" col col-6 col-md-3 main-cat-blocks-item"><a href="">
+			 <router-link
+                    :to="{name:'constaling'}">
+                    <div class="section-item">
+                        <img src="assets/img/cons-main.png">
+                        <p>Консалтинг</p>
+                    </div>
+                </router-link>
+		</a></div>
+	</div>
         <baners></baners>
         <div class="search fadeInUp wow">
             <UiFilter ref="index"></UiFilter>
@@ -31,7 +106,7 @@
             <p><b>ТурМедДрайвер - это здоровый и комфортный мир!</b></p>
         </div>
     </div>
-    <div class="container-fluid mp-services-blocks">
+    <!--<div class="container-fluid mp-services-blocks">
         <div class="row">
             <div class="col col-12 col-md-3">
                 <router-link
@@ -107,7 +182,7 @@
                 </router-link>
             </div>
         </div>
-    </div>
+    </div>-->
     <blog></blog>
 </template>
 
