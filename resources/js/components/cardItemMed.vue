a6f8c88498ef5433d4a1594f41b03fd203574342
klimVova <87908301+klimVova@users.noreply.github.com>
2024-02-19 16:42:59 +0300

    count

--- resources/js/components/cardItemMed.vue
+++ resources/js/components/cardItemMed.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="d-flex cardItem justify-content-between mt-md-3">
-        <p>{{title}}</p>
-        <p class="ml-3">{{price}}</p>
-        <div class="d-flex row">
-        <span class="plus" @click="emit('onClickPlusik')">+</span>
-        <p >{{ qty }}</p>
-        <span :class="qty<=1 ? 'disabled disabled-minus minus' : 'minus' " @click="emit('onClickMinus')">-</span>
+    <div class="d-flex cardItem justify-content-between mt-md-3 row">
+        <div class="col-8 d-flex">
+            <div class="col">
+            <p>{{title}}</p>
         </div>
-        <img @click="emit('onClickRemove')" class="itemRemove" src="assets/img/close.svg" alt="">
+        <div class="col text-center">
+            <p class="ml-3">{{price*qty}}</p>
+        </div>
+        </div>
+        <div class="col-4 d-flex">
+            <div class="col">
+            <div class="d-flex row">
+                    <span class="plus" @click="emit('onClickPlusik')">+</span>
+                    <p >{{ qty }}</p>
+                    <span :class="qty<=1 ? 'disabled disabled-minus minus' : 'minus' " @click="emit('onClickMinus')">-</span>
+            </div>
+        </div>    
+        <div class="col">
+            <img @click="emit('onClickRemove')" class="itemRemove" src="assets/img/close.svg" alt="">
+        </div>
+        </div>
     </div>
 </template>
 
